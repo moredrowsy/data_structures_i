2212a156098d8f35c95c232ea8632db85a918e03
Thuan Tang <thuantang@gmail.com>
2018-09-30 17:30:52 -0700

    Changed tests_main.cpp to catch.cpp and fixed Makefile for catch object generation

---
+++ catch_tests/catch.cpp
@@ -0,0 +1,3 @@
+// catch.cpp - provides linking catch2 framework for other test files
+#define CATCH_CONFIG_MAIN
+#include "../lib/catch.hpp"
--- catch_tests/tests_main.cpp
+++
@@ -1,3 +0,0 @@
-// tests_main.cpp
-#define CATCH_CONFIG_MAIN
-#include "../lib/catch.hpp"
