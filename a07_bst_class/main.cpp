4a5d71e5def7eb92e01ec43b696004e0021c27a7
Thuan Tang <thuantang@gmail.com>
2018-10-01 22:28:24 -0700

    Changed assignment 7's main to interactive tests

--- a07_bst_class/main.cpp
+++ a07_bst_class/main.cpp
@@ -1,163 +1,97 @@
+#include <cstdlib>  // rand()
+#include <ctime>    // time()
 #include <iostream>
 #include "../include/bst.h"
 
+void call_choice(bst::BST<int> &btree, char &c);
+void menu(char &c);
+void random_bst_test();
+
 int main() {
-    using namespace bst;
-
-    // create empty btree1 and insert with values
-    BST<int> btree1;
-
-    std::cout << "Default constructor: printing empty btree1:" << std::endl;
-    std::cout << btree1 << std::endl;
-
-    btree1.insert(50);
-    btree1.insert(70);
-    btree1.insert(55);
-    btree1.insert(60);
-    btree1.insert(40);
-    btree1.insert(30);
-    btree1.insert(45);
-
-    std::cout << std::endl;
-    std::cout << "Printing btree1 with inserted values:" << std::endl;
-    std::cout << btree1 << std::endl;
-
-    // copy constructor btree2 with btree1
-    BST<int> btree2(btree1);
-
-    std::cout << std::endl;
-    std::cout << "Copy constructor: printing btree2(btree1):" << std::endl;
-    std::cout << btree2 << std::endl;
-
-    btree2.insert(100);
-    btree2.insert(-1);
-
-    std::cout << std::endl;
-    std::cout << "Printing btree1 after modifying btree2:" << std::endl;
-    std::cout << btree1 << std::endl;
-
-    std::cout << std::endl;
-    std::cout << "Printing modified btree2:" << std::endl;
-    std::cout << btree2 << std::endl;
-
-    // assignment operator
-    btree2 = btree1;
-
-    std::cout << std::endl;
-    std::cout << "Assignment operator btree2 = btree1:" << std::endl;
-    std::cout << btree2 << std::endl;
-
-    // search begins here
-    bool is_search = false;
-    int target = 0;
-    bst_node::TreeNode<int> *found = nullptr;
-
-    // search 50
-    target = 50;
-    is_search = btree1.search(target, found);
-    std::cout << std::endl;
-
-    if(is_search) {
-        std::cout << "Search successful for target " << target
-                  << " and found = " << *found << std::endl;
-    } else {
-        std::cout << "Search for target " << target << " failed" << std::endl;
-    }
-
-    // search 70
-    target = 70;
-    is_search = btree1.search(target, found);
-    std::cout << std::endl;
-
-    if(is_search) {
-        std::cout << "Search successful for target " << target
-                  << " and found = " << *found << std::endl;
-    } else {
-        std::cout << "Search for target " << target << " failed" << std::endl;
-    }
-
-    // search 30
-    target = 30;
-    is_search = btree1.search(target, found);
-    std::cout << std::endl;
-
-    if(is_search) {
-        std::cout << "Search successful for target " << target
-                  << " and found = " << *found << std::endl;
-    } else {
-        std::cout << "Search for target " << target << " failed" << std::endl;
-    }
-
-    // search invalid 0
-    target = 0;
-    is_search = btree1.search(target, found);
-    std::cout << std::endl;
-
-    if(is_search) {
-        std::cout << "Search successful for target " << target
-                  << " and found = " << *found << std::endl;
-    } else {
-        std::cout << "Search for target " << target << " failed" << std::endl;
-    }
-
-    // erase here
-    bool is_erased = false;
-
-    // erase 50
-    target = 50;
-    is_erased = btree1.erase(target);
-
-    std::cout << std::endl;
-
-    if(is_erased)
-        std::cout << "Erasing successful for target " << target << std::endl;
-    else
-        std::cout << "Erasing target " << target << " failed" << std::endl;
-
-    std::cout << "Printing btree1 after erasing " << target << ":" << std::endl;
-    std::cout << btree1 << std::endl;
-
-    // erase 55
-    target = 55;
-    is_erased = btree1.erase(target);
-
-    std::cout << std::endl;
-
-    if(is_erased)
-        std::cout << "Erasing successful for target " << target << std::endl;
-    else
-        std::cout << "Erasing target " << target << " failed" << std::endl;
-
-    std::cout << "Printing btree1 after erasing " << target << ":" << std::endl;
-    std::cout << btree1 << std::endl;
-
-    // erase 70
-    target = 70;
-    is_erased = btree1.erase(target);
-
-    std::cout << std::endl;
-
-    if(is_erased)
-        std::cout << "Erasing successful for target " << target << std::endl;
-    else
-        std::cout << "Erasing target " << target << " failed" << std::endl;
-
-    std::cout << "Printing btree1 after erasing " << target << ":" << std::endl;
-    std::cout << btree1 << std::endl;
-
-    // erase 30
-    target = 30;
-    is_erased = btree1.erase(target);
-
-    std::cout << std::endl;
-
-    if(is_erased)
-        std::cout << "Erasing successful for target " << target << std::endl;
-    else
-        std::cout << "Erasing target " << target << " failed" << std::endl;
-
-    std::cout << "Printing btree1 after erasing " << target << ":" << std::endl;
-    std::cout << btree1 << std::endl;
+    random_bst_test();
 
     return 0;
+}
+
+void menu(char &c) {
+    std::cout << "[R]andom  [I]nsert  [C]clear  [S]earch  [E]rase      "
+                 "e[X]it: ";
+    std::cin >> c;
+}
+
+void random_bst_test() {
+    using namespace bst;
+
+    BST<int> btree;
+    char c;
+
+    do {
+        menu(c);                // display menu and get user choice
+        call_choice(btree, c);  // execute choice
+
+    } while(c != 'X' && c != 'x');
+}
+void call_choice(bst::BST<int> &btree, char &c) {
+    bool is_erased = false, is_found = false;
+    bst_node::TreeNode<int> *found = nullptr;
+    int i = 0;
+
+    switch(c) {
+        case 'C':
+        case 'c':
+            std::cout << "-- Clearing tree" << std::endl;
+            btree.clear();
+
+            break;
+        case 'E':
+        case 'e':
+
+            std::cout << "-- Erasing: ";
+            std::cin >> i;
+
+            is_erased = btree.erase(i);
+
+            if(is_erased)
+                std::cout << "Item erased: " << std::endl;
+            else
+                std::cout << "Item not in tree." << std::endl;
+            break;
+        case 'I':
+        case 'i':
+            std::cout << "-- Inserting: ";
+            std::cin >> i;
+
+            btree.insert(i);
+
+            break;
+
+        case 'R':
+        case 'r':
+            srand(time(nullptr));
+            i = rand() % 100;
+
+            std::cout << "-- Inserting random: " << i << std::endl;
+            btree.insert(i);
+
+            break;
+
+        case 'S':
+        case 's':
+            std::cout << "-- Searching: ";
+            std::cin >> i;
+
+            is_found = btree.search(i, found);
+
+            if(is_found)
+                std::cout << "Item found: " << *found << std::endl;
+            else
+                std::cout << "Item not found." << std::endl;
+
+            break;
+
+        default:
+            break;
+    }
+
+    if(c != 'X' && c != 'x') std::cout << btree << std::endl;
 }
